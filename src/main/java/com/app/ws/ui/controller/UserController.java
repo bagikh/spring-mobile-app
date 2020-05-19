e2f92042ab2ed1d3461d43235b778d7f3870c432
bagi <bagi4peace@gmail.com>
2020-05-20 01:15:45 +0900

    query added for get

--- src/main/java/com/app/ws/ui/controller/UserController.java
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("users") // http://localhost:8080/users
 public class UserController {
+	
+	@GetMapping
+	public String getUsers(@RequestParam(value="page")int page, 
+			@RequestParam(value="limit") int limit) {
+		return "get user was called with page="+page+"limit="+limit ;
+	}
 	
 	// Bind GET request
 	@GetMapping(path="/{userId}")
