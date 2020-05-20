c41a35dd1eadd7fde403f1ca4041f94ff7e8be58
bagikh <bagi4peace@gmail.com>
2020-05-20 10:38:55 +0900

    simple user controller with supporting response as xml

--- src/main/java/com/app/ws/ui/controller/UserController.java
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.ws.ui.model.response.UserRest;
+
 
 // Register this class as REST controller and enable receive/send http request/response
 @RestController
@@ -16,15 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 	
 	@GetMapping
-	public String getUsers(@RequestParam(value="page")int page, 
-			@RequestParam(value="limit") int limit) {
-		return "get user was called with page="+page+"limit="+limit ;
+	public String getUsers(@RequestParam(value="page", defaultValue="1")int page, 
+			@RequestParam(value="limit", defaultValue="50") int limit,
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort) {
+		return "get user was called with page="+page+"limit="+limit+"sort"+sort ;
 	}
 	
-	// Bind GET request
-	@GetMapping(path="/{userId}")
-	public String getUser(@PathVariable String userId) {
-		return "get user was called" + userId ;
+	// Bind GET request. Respond to user request as JSON or XML format
+	@GetMapping(path="/{userId}", produces= {
+			MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE
+			})
+	public UserRest getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@email.com");
+		returnValue.setFirstName("Bagi");
+		returnValue.setLastName("Kh");
+		
+		return returnValue ;
 	}
 	
 	@PostMapping
